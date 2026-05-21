8d2602776a59e46d9d5506ec82718aad5121d037
nononono649-lgtm <nosono649@gmail.com>
2026-05-21 18:19:57 +0900

    Update agent.ts

--- agent/agent.ts
+++ agent/agent.ts
@@ -1,4 +1,5 @@
 (globalThis as any).send = (globalThis as any)._pixelSend || (globalThis as any).send;
+(globalThis as any).recv = (globalThis as any)._pixelRecv || (globalThis as any).recv;
 "cut";
 // =============================================================================
 //  Offsets, agentSyms and patch values are injected at runtime from the host
